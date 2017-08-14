284a41f25fdd9a162bc6d2bb0628b66bd66016d5
sharsmita <30649076+sharsmita@users.noreply.github.com>
2017-08-14 16:56:22 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -5,12 +5,66 @@ var path = require('path');
 var app = express();
 app.use(morgan('combined'));
 
+
+var articleOne={
+  title:"Article One"  ,
+  heading :"Aricle one page",
+  date: "14 August",
+  content :
+  `
+        <p>
+            This is a new article about longitudes and latitudes.This is a new article about longitudes and latitudes.This is a new article about longitudes and latitudes.This is a new article about longitudes and latitudes.
+        </p>
+        <p>
+            This is a new article about longitudes and latitudes.This is a new article about longitudes and latitudes.This is a new article about longitudes and latitudes.This is a new article about longitudes and latitudes.
+        </p>
+
+    `
+};
+
+function createTemplate(data){
+
+var title = data.title;
+var date = data.date;
+var content = data.content;
+
+var htmlTemplate = 
+    `
+    <html>
+    <head>
+
+        <link href="/ui/style.css" rel="stylesheet" />
+
+    </head>
+
+    <body>
+        <a href="/">Home</a>
+        <div class="center">
+
+            <img src="/ui/madi.png" class="img-medium"/>
+
+        </div>
+        <h3>
+        ${title}
+        </h3>
+        <div>
+        ${date}
+        </div>
+        <div>
+        ${content}
+        </div>
+    </body>
+</html>`;
+return htmlTemplate;
+}
+
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 app.get('/article-one', function (req, res) {
-  //res.send('article one requested and will be displayed here');
-  res.sendFile(path.join(__dirname, 'ui', 'article-one.html'));
+ // res.send('article one requested and will be displayed here');
+  //res.sendFile(path.join(__dirname, 'ui', 'article-one.html'));
+   res.send(createTemplate(articleOne));
 });
 app.get('/article-two', function (req, res) {
   res.send('article two requested and will be displayed here');

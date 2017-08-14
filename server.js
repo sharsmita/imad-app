31c9ca01df7e959c89874acab0af6b05c2725aee
sharsmita <30649076+sharsmita@users.noreply.github.com>
2017-08-14 16:05:53 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -9,7 +9,8 @@ app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 app.get('/article-one', function (req, res) {
-  res.send('article one requested and will be displayed here');
+  //res.send('article one requested and will be displayed here');
+  res.sendFile(path.join(__dirname, 'ui', 'article-one.html'));
 });
 app.get('/article-two', function (req, res) {
   res.send('article two requested and will be displayed here');

5c5f9c4e1ed719a6ec0bee7d8456b8c3f60beff7
sharsmita <30649076+sharsmita@users.noreply.github.com>
2017-08-16 16:07:39 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,1 +1,22 @@
 console.log('Loaded!');
+var button = document.getElementById('counter');
+button.onClick = function(){
+ var request = new XMLHttpRequest();
+ 
+ request.onreadystatechange = function(){
+     
+     if(request.readystate === XMLHttpRequest.DONE)
+     {
+         if(request.status === 200)
+         {
+             var counter = request.responseText;
+             var span = document.getElementById('count');
+             span.innerHtml = counter.toString();
+         }
+     }
+ };
+ 
+ request.open('GET','http://sharsmita.imad.hasura-app.io/counter',true);
+ request.send(null);
+ 
+};
